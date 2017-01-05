158e40f81cbbcc3fd977398ea0bc80942dd5c5d1
brianlam38 <brian.lam38@gmail.com>
2017-01-05 16:05:20 +1100

    basic linux cmd notes

---
+++ BinaryHacking/notes.c
@@ -0,0 +1,23 @@
+/* BASIC COMMANDS */
+pwd 	         - print working directory // shows current location
+man <name>  	 - show manual page of <name> command
+ls [-l] [-a]	 - show content of the current location
+cat <path>		 - show content of the file at <path>
+cd <path>		 - change directory/location
+mkdir			 - make/create a new directory
+touch <path>	 - create a new empty file
+rm [-r] <path>	 - remove/delete file, [-r] recursive flag
+echo <text>		 - print <text>
+uname [-a]		 - display system information
+free [-h]		 - show memory usage		// only in LINUX
+df [-h]			 - show disk space usage
+ps [aux]		 - print process information
+id				 - show current user id
+hexdump			 - display data in hex numbers
+
+/* INSERTING CONTENT INTO FILE */
+echo <text> > <filename>
+echo follow the white rabbit > rabbit
+
+
+
