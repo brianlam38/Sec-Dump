2cfc476736ed9648a827909b410ca8234ded432a
brianlam38 <brian.lam38@gmail.com>
2017-03-18 18:35:30 +1100

    updating all files

---
+++ CTFs/EasyCTF_2017/Flag_Collection.c
@@ -0,0 +1,9 @@
+// Here's a collection of flags! I think you're looking for a specific one, though...
+
+int main(int argc, char *argv[]) {
+
+	// Convert image file into hex string.
+	// Search through the hex string for the flag
+
+	return EXIT_SUCCESS;
+}
