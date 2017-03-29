8e5bbdb65ef4394462fc86b4750b106b95c012da
brianlam38 <brian.lam38@gmail.com>
2017-03-30 00:28:21 +1100

    updating all files

--- Pwnable.kr/bof.c
+++ Pwnable.kr/bof.c
@@ -2,19 +2,24 @@
 #include <string.h>
 #include <stdlib.h>
 
-void func(int key){
-	char overflowme[32];
+void func(int key) {
+	char overflowme[32];	// 32 bytes length
 	printf("overflow me : ");
-	gets(overflowme);	// smash me!
-	if(key == 0xcafebabe){
+	gets(overflowme);		// smash me!
+	break;
+
+	if(key == 0xcafebabe){	// 3,405,691,582
 		system("/bin/sh");
 	}
-	else{
+	else {
 		printf("Nah..\n");
 	}
 }
 
 int main(int argc, char* argv[]){
-	func(0xdeadbeef);
+	func(0xdeadbeef);	// 3,735,928,559
 	return 0;
 }
+
+// Hint: "smash me @ gets(overflowme)"
+// 
